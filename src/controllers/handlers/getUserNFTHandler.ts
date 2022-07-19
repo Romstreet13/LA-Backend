34776901320cca8f03feb1fc5df5447c3f81a7ab
Serhii Stanislav <serhiistanislav@gmail.com>
2022-07-19 17:56:39 +0300

    adds NFTID to GET by userAddress

--- src/controllers/handlers/getUserNFTHandler.ts
+++ src/controllers/handlers/getUserNFTHandler.ts
@@ -1,16 +1,26 @@
 // @ts-nocheck
 import mintNFTService from '../../services/mintNFT.service';
+import NFTIDsService from '../../services/NFTIDs.service';
 
 const getUserNFTHandler = async data => {
   const mintNFTs = await mintNFTService.getAllMintNFTs();
-  let userNFT = [];
-
+  const allNFTIDs = await NFTIDsService.getAllNFTIDs();
   const keys = Object.keys(data);
+
+  let userNFT = [];
 
   if (keys[0] === 'userAddress') {
     for (let i = 0; i < mintNFTs.length; i += 1) {
+      let _NFTID = '';
+
+      for (let ii = 0; ii < allNFTIDs.length; ii += 1) {
+        allNFTIDs[ii].transactionHash === mintNFTs[i].transactionHash &&
+          (_NFTID = allNFTIDs[ii].nftId);
+      }
+
       mintNFTs[i].userAddress === data.userAddress &&
         userNFT.push({
+          nftId: _NFTID,
           merchant: mintNFTs[i].merchant,
           subscriptionId: mintNFTs[i].subscriptionId,
           userId: mintNFTs[i].userId,
