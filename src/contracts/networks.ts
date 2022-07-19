3e282e14004ed5b2c435343c11238b273e7dfc1c
Serhii Stanislav <serhiistanislav@gmail.com>
2022-07-19 09:36:36 +0300

    changes the rpc

--- src/contracts/networks.ts
+++ src/contracts/networks.ts
@@ -1,4 +1,6 @@
 export const networks = {
   bsc_main: 'https://bsc-dataseed.binance.org/',
-  mumbai: 'https://matic-mumbai.chainstacklabs.com',
+  mumbai:
+    'https://polygon-mumbai.g.alchemy.com/v2/LU2qtQ38feXJsLR9S6onsQe6vXYjsu2Y',
+  // mumbai: 'https://matic-mumbai.chainstacklabs.com',
 };
