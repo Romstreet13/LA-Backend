4c0b71717b57aabaaeed1fc4bc5126a7473fdd3d
Serhii Stanislav <serhiistanislav@gmail.com>
2022-07-21 12:40:26 +0300

    adds ropsten

--- src/contracts/networks.ts
+++ src/contracts/networks.ts
@@ -1,5 +1,6 @@
 export const networks = {
   bsc_main: 'https://bsc-dataseed.binance.org/',
+  ropsten: 'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
   mumbai:
     'https://polygon-mumbai.g.alchemy.com/v2/LU2qtQ38feXJsLR9S6onsQe6vXYjsu2Y',
 };
